e2c5dab85dff71ba3bdc8a0af77c5021de29dbb5
robinrakibul <robinrakibulhassan@gmail.com>
2022-05-02 13:28:14 +0600

    search individual item with id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const { query } = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 // middleware
@@ -32,6 +32,15 @@ async function run() {
             const items = await cursor.toArray();
             res.send(items);
         });
+
+        // searchId
+        app.get('/items/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+            const itemSearch = await itemsCollection.findOne(query);
+            res.send(itemSearch);
+        });
+        
 
     }
     finally {
